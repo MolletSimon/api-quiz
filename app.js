6422f011435b5e589f4721d7143097fc089e10cd
Simon Mollet <mollet.simon.pro@gmail.com>
2020-05-20 09:49:35 +0200

    init middleware, move pass to config

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
+const config = require('./config');
 
 //routes
 const quizRoutes = require('./routes/quiz');
 
 mongoose.connect(
-    'mongodb://simon-quiz:molimo76@cluster0-shard-00-00-twfrq.mongodb.net:27017,cluster0-shard-00-01-twfrq.mongodb.net:27017,cluster0-shard-00-02-twfrq.mongodb.net:27017/api-quiz?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
+    `mongodb://${config.MONGOUSER}:${config.MONGOPASS}@cluster0-shard-00-00-twfrq.mongodb.net:27017,cluster0-shard-00-01-twfrq.mongodb.net:27017,cluster0-shard-00-02-twfrq.mongodb.net:27017/api-quiz?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`,
     {useNewUrlParser: true,
         useUnifiedTopology: true})
     .then(() => console.log('Connexion à MongoDB réussie !'))
--- models/Quiz.js
+++ models/Quiz.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const quizSchema = mongoose.Schema({
+    userId: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
     title: {type: String, required: true},
     questions: [{
         title: {type: String, required: true},
